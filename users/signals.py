84eadfe99b5cd8a4ec20245b7383f97e1bc061c9
Xac47 <hasbulat69@gmail.com>
2024-03-25 01:36:55 +0300

    update file

--- users/signals.py
+++ users/signals.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
 from users.models import Profile
+
 
 # @receiver(post_save, sender=User)
 def createProfile(sender, instance, created, **kwargs):
@@ -25,6 +28,7 @@ def updateUser(sender, instance, **kwargs):
     user.email = profile.email
     user.save()
 
+
 # @receiver(post_delete, sender=Profile)
 def deleteProfile(sender, instance, **kwargs):
     user = instance.user
@@ -34,4 +38,3 @@ def deleteProfile(sender, instance, **kwargs):
 post_save.connect(createProfile, sender=User)
 post_save.connect(updateUser, sender=Profile)
 post_delete.connect(deleteProfile, sender=Profile)
-
