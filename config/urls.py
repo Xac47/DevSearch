508b3995671fa204d0179b240c56ce2e37e7b7f3
Xac47 <hasbulat69@gmail.com>
2024-03-25 04:03:29 +0300

    add ckeditor

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
 ]
